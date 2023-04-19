b9d05ad09ff6b686c8262bfb7496dfa196261c0a
Artem Rozycki <126414299+ssmqd@users.noreply.github.com>
2023-04-19 07:15:00 +0200

    Delete other.py

--- hendlers/other.py
+++
@@ -1,7 +0,0 @@
-# from aiogram import types, Dispatcher
-# from create_bot import bot
-# from aiogram.types import ReplyKeyboardRemove
-# from aiogram.dispatcher.filters import Text
-#
-#
-# def register_hendlers_other(dp: Dispatcher):
